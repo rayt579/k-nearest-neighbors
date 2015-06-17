a02e0ff637cd44b372bcc4c80d6c097f4a1d04e6
Ray <ray@ray.attlocal.net>
2015-06-17 16:12:24 -0700

    Performed on MNIST dataset

--- knn.py
+++ knn.py
@@ -11,17 +11,14 @@ Returns array containing the k-nearest neighbors of point x
 '''
 def knn_x(data, x, k):	
 	n,m = data.shape
-	
+
 	# To calculate Euclidean distance efficiently, 
 	# use numpy subtraction/addition ops. 
-
 	x_data = np.tile(x,(n,1))	
 	dist_matrix = LA.norm((x_data - data),axis=1)
-	#print dist_matrix
-
+	
 	# Return an array containing the indexes k closest neighbors
 	neighbors_indexes = np.argsort(dist_matrix)[:k]
-	print "Neighbor index:", neighbors_indexes
 	return neighbors_indexes
 
 '''
@@ -32,7 +29,6 @@ def knn_clf_x(neighbors, true_labels):
 	for idx in neighbors: 
 		votes.append(true_labels[idx])
 	votes = np.array(votes,dtype ='int')
-	print "Votes: ", votes
 	return np.argmax(np.bincount(votes))
 
 '''
@@ -58,22 +54,34 @@ def calc_error(true_labels,pred_labels):
 	return  (miscount/n)
 
 '''
-Gets the training and test error from the knn clf. 
+Gets the training and validation error from the knn clf. 
 '''
 def main():
-	train = np.genfromtxt('toy_data/train_data.txt')
-	test = np.genfromtxt('toy_data/test_data.txt')
-	
+	f = open('output.txt','w')
+	train = np.genfromtxt('data/train_data.txt')
+	test = np.genfromtxt('data/test_data.txt')
+	val = np.genfromtxt('data/val_data.txt')
+
 	# Parse data for separating training labels and dataset
 	n_feat = train[0].size
 	train_data = train[:,:-1]
 	train_labels = train[:,n_feat-1]
 	test_data = test[:,:-1]
 	test_labels = test[:,n_feat-1]
+	val_data = val[:,:-1]
+	val_labels = val[:,n_feat-1]
 
-	# Print test/training error and predictions for the classifier 
-	preds = knn_algorithm(train_data,train_labels,test_data,3)
-	print preds
-	
+	# Print training + validation error for the classifier 
+	k_neighbors = [1,3,5,11,16,21]
+	for k in k_neighbors:
+		preds_train = knn_algorithm(train_data,train_labels,train_data,k)
+		preds_val = knn_algorithm(train_data,train_labels,val_data,k)
+		preds_test = knn_algorithm(train_data,train_labels,test_data,k)
+		f.write("%s-neighbors: \n" % k)
+		f.write("Training error: %s \n" % (calc_error(train_labels,preds_train)))
+		f.write("Validation error: %s \n" % (calc_error(val_labels,preds_val)))
+		f.write("Test error: %s \n" % (calc_error(test_labels,preds_test)))
+		f.write("\n")
+
 if __name__ == '__main__':
 	main()
